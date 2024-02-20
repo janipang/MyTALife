8303d33e27611bced55ff0de64ea08b91a4cf5f0
Pang Janista <93817826+janipang@users.noreply.github.com>
2024-02-20 12:11:20 +0700

    christmas tree solution

---
+++ christmas_tree.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+int main(){
+    int n;
+    printf("input n: ");
+    scanf("%d", &n);
+
+    int row=0,counter,adder=1;;
+    for (counter=0; counter < n; counter+=adder,adder++){
+        row++;
+    }
+    printf("row: %d\n",row);
+
+    int lwing, rwing;
+    if (counter > n){
+        if (row % 2 == 0){
+            lwing = row - 2; rwing = row - 1;
+        }
+        else {
+            rwing = row - 2; lwing = row - 1;
+        }
+    }
+    else if (counter == n){
+        if (row % 2 == 0){
+            lwing = row ; rwing = row - 1;
+        }
+        else {
+            rwing = row ; lwing = row - 1;
+        }
+    }
+    printf("lwing: %d, rwing: %d\n", lwing, rwing);
+
+    int alpha = 0;
+    int printed = 0;
+    for (int i = 0; i < row; i++){
+        for (int j = 0; j < (lwing + rwing - 1); j++){
+            if((i % 2 == 0) && (printed < n) && (j >= lwing - 1 && j < lwing + i)){
+                printf("%c ",'A'+((alpha++))%26);
+                printed++;
+            }
+            else if((i % 2 != 0) && (printed < n) && (j < lwing && j >= (lwing - i) - 1)){
+                printf("%c ",'A'+((alpha++))%26);
+                printed++;
+            }
+            else{
+                printf("_ ");
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
